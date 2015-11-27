82f9b60a44ef1c75154374deadea8a25637a8855
Remo Inderbitzin <remo@inderbitzin.org>
2015-11-27 16:11:54 +0100

    fix: remove sliding header

--- js/navbar_affix.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $('#top-nav').addClass('affix');
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $('#top-nav').removeClass('affix');
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
