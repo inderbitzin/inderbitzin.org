49ff882358c06dfb84dca97f6e91b36158732460
Remo Inderbitzin <remo@inderbitzin.org>
2015-12-09 01:20:02 +0100

    feat: switch to gmaps.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,42 @@
-function initialize() {
-  var latlng = new google.maps.LatLng(46.993844, 8.606245);
-  var myOptions = {
-    zoom: 16,
-    center: latlng,
-    scrollwheel: false,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+$(document).ready(function() {
+	$('#nav').onePageNav({
+    scrollOffset: 70,
+		scrollSpeed: 750,
+	});
+});
 
-  var map = new google.maps.Map(document.getElementById("map-contact"),
-  myOptions);
+map();
 
-  var marker = new google.maps.Marker({
-    position: latlng,
-    map: map,
-  });
+function map() {
+
+    var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
+    map = new GMaps({
+        el: '#map-contact',
+        lat: 46.993844,
+        lng: 8.606245,
+        zoomControl: true,
+        zoomControlOpt: {
+            style: 'SMALL',
+            position: 'TOP_LEFT'
+        },
+        panControl: false,
+        streetViewControl: false,
+        mapTypeControl: false,
+        overviewMapControl: false,
+        scrollwheel: false,
+        draggable: false,
+        styles: styles
+    });
+
+    var image = 'images/ii_marker.png';
+
+    map.addMarker({
+      lat: 46.993844,
+      lng: 8.606245,
+        icon: image,
+        title: 'Inderbitzin Informatik',
+        infoWindow: {
+            content: '<p>Inderbitzin Informatik</p>'
+        }
+    });
 }
-
-google.maps.event.addDomListener(window, "load", initialize);
